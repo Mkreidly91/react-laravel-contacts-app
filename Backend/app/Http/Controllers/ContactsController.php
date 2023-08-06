c97b12ef8ca1aee328b07407ed1df7f9976b6260
Mostafa Kreidly <mkreidly91@gmail.com>
2023-08-06 13:12:34 +0300

    <BE> handled base64 from backend

--- Backend/app/Http/Controllers/ContactsController.php
+++ Backend/app/Http/Controllers/ContactsController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Contact;
-use Symfony\Component\Mime\Message;
+
+use Illuminate\Support\Facades\Storage;
 
 class ContactsController extends Controller
 {
@@ -12,16 +13,29 @@ class ContactsController extends Controller
     {
         try {
             $contacts = Contact::all();
+            foreach ($contacts as $contact) {
+
+                $img_url = $contact->image_url;
+                if (Storage::disk('public')->exists($img_url)) {
+
+                    $image = Storage::disk('public')->get($img_url);
+                    $mimeType = Storage::disk('public')->mimeType($img_url);
+                    $contact->image_url = 'data:' . $mimeType . ';base64,' . base64_encode($image);
+                    ;
+                }
+            }
+
             return response()->json([
                 'status' => 'success',
                 'contacts' => $contacts,
             ], 200);
-        } catch (\Throwable $th) {
+        } catch (\Exception $e) {
             return response()->json([
                 'status' => 'error',
-                'message' => $th,
+                'message' => $e->getMessage(),
             ], 500);
         }
+
     }
 
     function add(Request $request)
@@ -40,7 +54,22 @@ class ContactsController extends Controller
         $contact->name = $request->name;
         $contact->number = $request->number;
         $contact->location = $request->location;
-        $contact->image_url = $request->img;
+
+        // Decode base 64
+        $image_64 = $request->img;
+
+        if ($image_64) {
+            $extension = explode('/', explode(':', substr($image_64, 0, strpos($image_64, ';')))[1])[1];
+            $replace = substr($image_64, 0, strpos($image_64, ',') + 1);
+            $image = str_replace($replace, '', $image_64);
+            $image = str_replace(' ', '+', $image);
+            $filename = uniqid() . '.' . $extension;
+            $image_url = 'images/' . $filename;
+            Storage::disk('public')->put('images/' . $filename, base64_decode($image));
+            $contact->image_url = $image_url;
+        }
+
+
 
         try {
             $contact->save();
@@ -53,7 +82,6 @@ class ContactsController extends Controller
             ], 500);
         }
 
-
     }
 
     function edit(Request $request, $id)
@@ -62,36 +90,57 @@ class ContactsController extends Controller
             "name" => "nullable|string",
             "number" => "nullable|string",
             'location' => 'nullable|string|regex:/^[a-zA-Z0-9\s\-,.]+$/',
-
         ]);
 
         $contact = Contact::find($id);
 
-
         foreach ($request->all() as $field => $value) {
-
-            if ($value) {
-                $contact[$field] = $value;
+            if ($field != 'img') {
+                if ($value) {
+                    $contact[$field] = $value;
+                }
             }
         }
+        // Decode base 64
+        if ($request->img) {
+
+            if (!empty($imageData) && base64_decode($imageData, true)) {
+                // Extract the image extension from the data
+                $imageExtension = explode('/', explode(':', substr($imageData, 0, strpos($imageData, ';')))[1])[1];
+
+                // Generate a unique filename for the image
+                $filename = uniqid() . '.' . $imageExtension;
+
+                // Decode the base64 data
+                $decodedImage = base64_decode($imageData);
+                $image_url = 'images/' . $filename;
+                $contact->image_url = $image_url;
+                // Save the image to the storage (assuming you are using the public disk)
+                Storage::disk('public')->put('images/' . $filename, $decodedImage);
+            }
+        }
+
         $contact->save();
 
         return response()->json([
             'message' => 'success',
             'updatedContact' => Contact::find($id)
         ], 200);
-
     }
 
     function delete($id)
     {
         try {
             Contact::destroy($id);
-        } catch (\Throwable $th) {
             return response()->json([
                 'message' => 'user successfully deleted',
 
             ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => $th,
+
+            ], 500);
         }
 
     }
